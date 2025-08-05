61a95cf3b8d1603ee9a00cb46fcc5caddb1f2d43
alejandro <alejandrobojaca1@gmail.com>
2025-08-05 17:30:04 -0500

    feat: kafka basic code

--- kafka/basic/consumer.py
+++ kafka/basic/consumer.py
@@ -1,19 +1,14 @@
 from kafka import KafkaConsumer, TopicPartition
+import json
+
 
 def main():
-    consumer = KafkaConsumer('my_favorite_topic')
-    for msg in consumer:
-        print (msg)
+    consumer = KafkaConsumer(bootstrap_servers='localhost:29092')
+    consumer.subscribe(['my_favorite_topic'])
 
-    # join a consumer group for dynamic partition assignment and offset commits
-    consumer = KafkaConsumer('my_favorite_topic', group_id='my_favorite_group')
     for msg in consumer:
-        print (msg)
-
-    # manually assign the partition list for the consumer
-    # consumer = KafkaConsumer(bootstrap_servers='localhost:1234')
-    # consumer.assign([TopicPartition('foobar', 2)])
+        print(msg.value)
+        print(json.loads(msg.value)['foo'])
 
 if __name__ == "__main__":
-    print('here')
     main()
--- kafka/basic/producer.py
+++ kafka/basic/producer.py
@@ -1,19 +1,11 @@
 from kafka import KafkaProducer
 import json
 
+
 def main():
-    producer = KafkaProducer(bootstrap_servers='localhost:2181', value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-
+    producer = KafkaProducer(bootstrap_servers='localhost:29092', value_serializer=lambda v: json.dumps(v).encode('utf-8'))
     for _ in range(10):
-        producer.send('my_favorite_topic', {'foo': 'bar'})
-
-    # Block until a single message is sent (or timeout)
-    # future = producer.send('foobar', b'another_message')
-    # result = future.get(timeout=60)
-
-
-
+        producer.send('my_favorite_topic', {"foo": "foo"})
 
 if __name__ == "__main__":
-    print('here')
     main()  
